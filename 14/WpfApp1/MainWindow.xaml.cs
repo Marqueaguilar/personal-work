55b1913a09fe9185f2a7a4d1e643c49ffa3856d9
Marqueaguilar <70530429+Marqueaguilar@users.noreply.github.com>
2021-10-05 11:46:11 -0500

    Add files via upload

---
+++ 14/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //DateTime myDate = DateTime.Now;
+
+            //LabelDate.Content = myDate.ToString();
+            //LabelDate.Content = myDate.ToLongDateString();
+            //LabelDate.Content = myDate.ToLongTimeString();
+            //LabelDate.Content = myDate.ToShortDateString();
+            //LabelDate.Content = myDate.ToShortTimeString();
+
+            //LabelDate.Content = myDate.AddDays(2).ToString();
+            //LabelDate.Content = myDate.AddMonths(-2).ToString();
+
+            //LabelDate.Content = myDate.IsDaylightSavingTime().ToString();
+            //LabelDate.Content = myDate.DayOfWeek.ToString();
+
+            //DateTime myDate = DateTime.Parse("12/09/1963");
+
+            DateTime myDate = new DateTime(1963, 12, 9);
+            LabelDate.Content = myDate.ToString();
+
+
+
+
+
+
+
+        }
+    }
+}
