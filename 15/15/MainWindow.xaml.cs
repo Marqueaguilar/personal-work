50020d4f4eeff70fbb5251c544753f2ab1290bb4
Marqueaguilar <70530429+Marqueaguilar@users.noreply.github.com>
2021-10-05 13:36:30 -0500

    Add files via upload

---
+++ 15/15/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _15
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // https://is.gd/timespan
+            //Days.Hours;Minutes:Seconds.Milliseconds
+
+            TimeSpan myTimeSpan = TimeSpan.Parse("1.6:35:22.6");
+
+            DateTime myBirthday = DateTime.Parse("10/17/2004");
+
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+
+             = myAge
+
+        }
+    }
+}
---
+++ 15/15/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // http://id.gd/timespan
+            //Days.Hours:Minuits:Seconds.Milliseconds
+
+            
+            TimeSpan myTimeSpan = TimeSpan.Parse("1.6:35:22.6");
+            
+            DateTime myBirthday = DateTime.Parse("10/17/2004");
+            
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+
+            // LabelResult.Content = myAge.Hours.ToString();
+
+            LabelResult.Content = myAge.TotalDays.ToString();
+
+        }
+    }
+}
