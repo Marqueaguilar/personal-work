a305fa70959cee4f39aac38c8a94b99c8236afe7
Marqueaguilar <70530429+Marqueaguilar@users.noreply.github.com>
2021-11-18 11:12:18 -0600

    Add files via upload

---
+++ 20/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20
+{
+   
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string result = string.Format("Thank you, {0} your soical security number is {1:000-00-0000}, {2} is your phone number.", namewrite.Text , ssnonum.Text , phonenum.Text );
+            LabelResult.Content = result;     
+        }
+    }
+}
